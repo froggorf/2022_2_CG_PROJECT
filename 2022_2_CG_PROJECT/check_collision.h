4925a6938092effe3bc7c6cfbf410b4e3ad47b0f
froggorf <kjggr123@gmail.com>
2022-12-07 02:33:37 +0900

    AABB충돌 추가

--- 2022_2_CG_PROJECT/check_collision.h
+++ 2022_2_CG_PROJECT/check_collision.h
@@ -1,4 +1,11 @@
+#ifndef __CHECK_COLLISION_H__
+#define __CHECK_COLLISION_H__
+
+
+
 #include "stdafx.h"
 #include "cube.h"
 
 GLboolean CheckAABB(Cube cube1, Cube cube2);
+
+#endif // !__CHECK_COLLISION_H__
