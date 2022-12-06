fde6fb53773f4ec8b40d438c41c5cdb2a82cb4c8
froggorf <kjggr123@gmail.com>
2022-12-07 02:32:14 +0900

    이미지 관련 함수 헤더 추가

---
+++ 2022_2_CG_PROJECT/ImageManager.cpp
@@ -0,0 +1,16 @@
+#include "ImageManager.h"
+
+void LoadTexture(GLuint& texture, const char* img_name) {
+	int w, h, channel;
+	glGenTextures(1, &texture); //--- 텍스처 생성
+	glBindTexture(GL_TEXTURE_2D, texture); //--- 텍스처 바인딩
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT); //--- 현재 바인딩된 텍스처의 파라미터 설정하기
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	unsigned char* data = stbi_load(img_name, &w, &h, &channel, 4); //--- 텍스처로 사용할 비트맵 이미지 로드하기
+	std::cout << data << std::endl;
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); //---텍스처 이미지 정의	https://stackoverflow.com/questions/72057406/access-violation-in-nvoglv64-dll-when-using-opengl 에서 오류 수정
+	stbi_image_free(data);
+}
--- 2022_2_CG_PROJECT/ImageManager.h
+++ 2022_2_CG_PROJECT/ImageManager.h
@@ -1,6 +1,18 @@
-//https://heinleinsgame.tistory.com/9
+#ifndef __IMAGEMANAGER_H__
+#define __IMAGEMANAGER_H__
 #include "stdafx.h"
 
-//class ImageManager {
-//public:
-//};
+void LoadTexture(GLuint& texture, const char* img_name);
+//
+//void InitTexture()
+//{
+//	//stbi_set_flip_vertically_on_load(true); //이미지 뒤집히면 넣기
+//	LoadTexture(box_texture, "aaaa.png");
+//	//stbi_set_flip_vertically_on_load(true); //이미지 뒤집히면 넣기
+//	LoadTexture(sky_texture[0], "skymap_0.png");
+//	LoadTexture(sky_texture[1], "skymap_1.png");
+//	LoadTexture(sky_texture[2], "skymap_2.png");
+//	LoadTexture(sky_texture[3], "skymap_3.png");
+//}
+
+#endif // !__IMAGEMANAGER_H__
