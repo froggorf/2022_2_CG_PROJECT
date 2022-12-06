cacecfc5913c666d97aee6da17fc942c86521739
froggorf <kjggr123@gmail.com>
2022-12-07 02:32:38 +0900

    stdafx헤더에 이미지 헤더 추가

--- 2022_2_CG_PROJECT/stdafx.h
+++ 2022_2_CG_PROJECT/stdafx.h
@@ -2,18 +2,19 @@
 #define __STDAFX_H__
 
 #define _CRT_SECURE_NO_WARNINGS
-#define STB_IMAGE_IMPLEMENTATION
 
 #include <iostream>
 #include <random>
 #include <vector>
+#include <fstream>
 #include <gl/glew.h> 
 #include<gl/freeglut.h>
 #include <gl/freeglut_ext.h>
 #include <gl/glm/glm/glm.hpp>
 #include <gl/glm/glm/ext.hpp>
 #include <gl/glm/glm/gtc/matrix_transform.hpp>
-//#include "stb_image.h"
+#include "stb_image.h"
+
 
 enum XYZ { X = 0, Y, Z };
 enum RGB { R = 0, G, B };
