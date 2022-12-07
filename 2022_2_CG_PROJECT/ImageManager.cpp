32135089ce07d38a27c4212246f080ca5fa168ea
froggorf <kjggr123@gmail.com>
2022-12-07 12:54:56 +0900

    사진 뒤집기 추가

--- 2022_2_CG_PROJECT/ImageManager.cpp
+++ 2022_2_CG_PROJECT/ImageManager.cpp
@@ -1,6 +1,7 @@
 #include "ImageManager.h"
 
 void LoadTexture(GLuint& texture, const char* img_name) {
+	stbi_set_flip_vertically_on_load(true);
 	int w, h, channel;
 	glGenTextures(1, &texture); //--- 텍스처 생성
 	glBindTexture(GL_TEXTURE_2D, texture); //--- 텍스처 바인딩
@@ -9,7 +10,7 @@ void LoadTexture(GLuint& texture, const char* img_name) {
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	unsigned char* data = stbi_load(img_name, &w, &h, &channel, 4); //--- 텍스처로 사용할 비트맵 이미지 로드하기
-	std::cout << data << std::endl;
+	
 	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data); //---텍스처 이미지 정의	https://stackoverflow.com/questions/72057406/access-violation-in-nvoglv64-dll-when-using-opengl 에서 오류 수정
 	stbi_image_free(data);
