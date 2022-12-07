14d8b016d92c5450acb8ad82d576ab54cd4d1816
froggorf <kjggr123@gmail.com>
2022-12-07 12:55:15 +0900

    location 위치 수정

--- 2022_2_CG_PROJECT/vertex_texture.glsl
+++ 2022_2_CG_PROJECT/vertex_texture.glsl
@@ -1,7 +1,7 @@
 #version 330 core
-layout (location = 0) in vec3 vPos;
-layout (location = 1) in vec3 vNormal;
-layout (location = 2) in vec2 vTexCoord;
+layout (location = 3) in vec3 vPos;
+layout (location = 4) in vec3 vNormal;
+layout (location = 5) in vec2 vTexCoord;
 
 out vec3 FragPos;
 out vec3 Normal;
