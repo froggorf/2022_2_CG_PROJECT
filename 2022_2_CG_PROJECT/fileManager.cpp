8cb52d71e6ba6477b06f5d56c29d1673114bf94b
froggorf <kjggr123@gmail.com>
2022-12-08 23:37:27 +0900

    수정 최종본

--- 2022_2_CG_PROJECT/fileManager.cpp
+++ 2022_2_CG_PROJECT/fileManager.cpp
@@ -1,81 +1,81 @@
 
 #include "fileManager.h"
 
-void loadCube(std::vector<Cube> &vec) {
-	std::ifstream ifs("Vertex.txt");
-	if (!ifs.fail()) {
-		while (!ifs.eof()) {
-			glm::vec3 scale, trans, rotate;
-			char c;
-			ifs >> scale[0] >> c >> scale[1] >> c >> scale[2];
-			ifs >> trans[0] >> c >> trans[1] >> c >> trans[2];
-			ifs >> rotate[0] >> c >> rotate[1] >> c >> rotate[2];
-			Cube temp = Cube(scale, trans, rotate);
-			vec.push_back(temp);
-		}
-		ifs.close();
-	}
+void loadCube(std::vector<Cube>& vec) {
+    std::ifstream ifs("Vertex.txt");
+    if (!ifs.fail()) {
+        while (!ifs.eof()) {
+            glm::vec3 scale, trans, rotate;
+            char c;
+            ifs >> scale[0] >> c >> scale[1] >> c >> scale[2];
+            ifs >> trans[0] >> c >> trans[1] >> c >> trans[2];
+            ifs >> rotate[0] >> c >> rotate[1] >> c >> rotate[2];
+            Cube temp = Cube(scale, trans, rotate);
+            vec.push_back(temp);
+        }
+        ifs.close();
+    }
 }
 
-void loadStage1(std::vector<Cube*> &vec) {
-	std::ifstream ifs("resource/Map/stage1.txt");
-	if (!ifs.fail()) {
-		int i = 0;
-		while (!ifs.eof()) {
-			glm::vec3 scale, trans, rotate;
-			char c;
-			Cube* temp;
-			ifs >> scale[0] >> c >> scale[1] >> c >> scale[2];
-			ifs >> trans[0] >> c >> trans[1] >> c >> trans[2];
-			ifs >> rotate[0] >> c >> rotate[1] >> c >> rotate[2];
-			if (i < 4) {
-				temp = new Ground(scale, trans, rotate);
-				vec.push_back(temp);
-			}
-			else if (i >= 4 and i < 6) {
-				temp = new Door(scale, trans, rotate);
-				vec.push_back(temp);
-			}//TODO: 문
-			else if (i >= 6 and i < 10) {
-				temp = new Pipe(scale, trans, rotate);
-				vec.push_back(temp);
-			}//TODO: 관
-			else if (i >= 19 and i < 37) {
-				temp = new Block(scale, trans, rotate);
-				vec.push_back(temp);
-			}//TODO: 블럭
-			else {
-				switch (i)
-				{
-				case 10:
-					temp = new MysteryBlock(scale, trans, rotate, 100);
-					break;
-				case 12: case 14: case 37:
-					temp = new MysteryBlock(scale, trans, rotate, 1);
-					break;
-				default:
-					temp = new Brick(scale, trans, rotate);
-					break;
-				}
-				vec.push_back(temp);
-			}
-			i++;
-		}
-		ifs.close();
-		vec.pop_back();
-	}
+void loadStage1(std::vector<Cube*>& vec) {
+    std::ifstream ifs("resource/Map/stage1.txt");
+    if (!ifs.fail()) {
+        int i = 0;
+        while (!ifs.eof()) {
+            glm::vec3 scale, trans, rotate;
+            char c;
+            Cube* temp;
+            ifs >> scale[0] >> c >> scale[1] >> c >> scale[2];
+            ifs >> trans[0] >> c >> trans[1] >> c >> trans[2];
+            ifs >> rotate[0] >> c >> rotate[1] >> c >> rotate[2];
+            if (i < 4) {
+                temp = new Ground(scale, trans, rotate);
+                vec.push_back(temp);
+            }
+            else if (i >= 4 and i < 6) {
+                temp = new Door(scale, trans, rotate);
+                vec.push_back(temp);
+            }
+            else if (i >= 6 and i < 10) {
+                temp = new Pipe(scale, trans, rotate);
+                vec.push_back(temp);
+            }
+            else if (i >= 19 and i < 37) {
+                temp = new Block(scale, trans, rotate);
+                vec.push_back(temp);
+            }
+            else {
+                switch (i)
+                {
+                case 10:
+                    temp = new MysteryBlock(scale, trans, rotate, 100);
+                    break;
+                case 12: case 14: case 37:
+                    temp = new MysteryBlock(scale, trans, rotate, 1);
+                    break;
+                default:
+                    temp = new Brick(scale, trans, rotate);
+                    break;
+                }
+                vec.push_back(temp);
+            }
+            i++;
+        }
+        ifs.close();
+        vec.pop_back();
+    }
 }
 
 void loadBlock() {
-	std::ifstream ifs("Vertex.txt");
-	if (!ifs.fail()) {
-		while (!ifs.eof()) {
-			glm::vec3 scale, trans, rotate;
-			char c;
-			ifs >> scale[0] >> c >> scale[1] >> c >> scale[2];
-			ifs >> trans[0] >> c >> trans[1] >> c >> trans[2];
-			ifs >> rotate[0] >> c >> rotate[1] >> c >> rotate[2];
-			Cube* temp = new Cube(scale, trans, rotate);
-		}
-	}
+    std::ifstream ifs("Vertex.txt");
+    if (!ifs.fail()) {
+        while (!ifs.eof()) {
+            glm::vec3 scale, trans, rotate;
+            char c;
+            ifs >> scale[0] >> c >> scale[1] >> c >> scale[2];
+            ifs >> trans[0] >> c >> trans[1] >> c >> trans[2];
+            ifs >> rotate[0] >> c >> rotate[1] >> c >> rotate[2];
+            Cube* temp = new Cube(scale, trans, rotate);
+        }
+    }
 }
