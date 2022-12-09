582cd73bcf8adab4117d062452e5ac7915382846
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-12-09 13:03:08 +0900

    이것도

--- 2022_2_CG_PROJECT/door.h
+++ 2022_2_CG_PROJECT/door.h
@@ -1,17 +1,20 @@
 #ifndef __DOOR_H__
 #define __DOOR_H__
 #include "cube.h"
+#include <string>
 
 class Door : public Cube {
 public:
 	Cube boundingBox;
 	GLuint VAO, VBO[3];
+	GLuint frame;
 
 	Door();
 	Door(glm::vec3 scale, glm::vec3 trans, glm::vec3 rotate);
 	~Door();
 
 	GLvoid Init();
+	GLvoid update();
 	virtual GLvoid InitBuffer();
 	virtual GLvoid draw();
 };
