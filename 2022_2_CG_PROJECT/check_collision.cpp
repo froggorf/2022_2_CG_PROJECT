a4b23b42f7c3f2c6d70ade1767b69eaaa86c7270
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-12-09 16:43:58 +0900

    2D용 충돌처리 함수

--- 2022_2_CG_PROJECT/check_collision.cpp
+++ 2022_2_CG_PROJECT/check_collision.cpp
@@ -2,7 +2,7 @@
 
 GLboolean CheckAABB(Cube cube1, Cube cube2) {
 	//x축 충돌 체크
-	
+
 	if (!(cube1.trans.x - 0.5 * cube1.scale.x < cube2.trans.x + 0.5 * cube2.scale.x
 		&& cube1.trans.x + 0.5 * cube1.scale.x>cube2.trans.x - 0.5 * cube2.scale.x)) {
 		return false;
@@ -15,6 +15,22 @@ GLboolean CheckAABB(Cube cube1, Cube cube2) {
 
 	if (!(cube1.trans.z - 0.5 * cube1.scale.z < cube2.trans.z + 0.5 * cube2.scale.z
 		&& cube1.trans.z + 0.5 * cube1.scale.z>cube2.trans.z - 0.5 * cube2.scale.z)) {
+		return false;
+	}
+
+	return true;
+
+}
+
+GLboolean CheckAABB_2D(Cube cube1, Cube cube2) {
+	//x축 충돌 체크
+	if (!(cube1.trans.x - 0.5 * cube1.scale.x < cube2.trans.x + 0.5 * cube2.scale.x
+		&& cube1.trans.x + 0.5 * cube1.scale.x>cube2.trans.x - 0.5 * cube2.scale.x)) {
+		return false;
+	}
+
+	if (!(cube1.trans.y - 0.5 * cube1.scale.y < cube2.trans.y + 0.5 * cube2.scale.y
+		&& cube1.trans.y + 0.5 * cube1.scale.y>cube2.trans.y - 0.5 * cube2.scale.y)) {
 		return false;
 	}
 
--- 2022_2_CG_PROJECT/check_collision.h
+++ 2022_2_CG_PROJECT/check_collision.h
@@ -7,5 +7,6 @@
 #include "cube.h"
 
 GLboolean CheckAABB(Cube cube1, Cube cube2);
+GLboolean CheckAABB_2D(Cube cube1, Cube cube2);
 
 #endif // !__CHECK_COLLISION_H__
