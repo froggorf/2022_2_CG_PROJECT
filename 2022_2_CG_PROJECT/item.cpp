2657e432ea753dc1001089a490953cdef4ce333f
Ko-jung <69414186+Ko-jung@users.noreply.github.com>
2022-12-09 22:34:50 +0900

    아이템 클래스

---
+++ 2022_2_CG_PROJECT/item.cpp
@@ -0,0 +1,17 @@
+#include "item.h"
+
+Item::Item() { };
+Item::Item(glm::vec3 scale, glm::vec3 trans, glm::vec3 rotate) { };
+Item::~Item() { };
+
+GLvoid Item::Init() {
+
+}
+GLvoid Item::InitBuffer() {
+}
+
+GLvoid Item::update() {
+}
+
+GLvoid Item::draw() {
+}
---
+++ 2022_2_CG_PROJECT/item.h
@@ -0,0 +1,25 @@
+#ifndef __ITEM_H__
+#define __ITEM_H__
+#include "cube.h"
+#include "check_collision.h"
+
+class Item : public Cube {
+public:
+	Cube boundingBox;
+	GLuint VAO, VBO[3];
+	GLuint frame;
+
+	GLboolean dead;
+	GLuint dir;
+
+	Item();
+	Item(glm::vec3 scale, glm::vec3 trans, glm::vec3 rotate);
+	~Item();
+
+	GLvoid Init();
+	virtual GLvoid update();
+	virtual GLvoid InitBuffer();
+	virtual GLvoid draw();
+};
+
+#endif
