f0ee33fdcda613676843d403db9a2cdc38400648
froggorf <kjggr123@gmail.com>
2022-12-10 00:15:54 +0900

    프래그먼트 쉐이더 수정

--- 2022_2_CG_PROJECT/fragment_texture.glsl
+++ 2022_2_CG_PROJECT/fragment_texture.glsl
@@ -9,6 +9,9 @@ uniform sampler2D outTexture;
 
 void main(){
 	FragColor = texture(outTexture,TexCoord);
-	if(vec3(FragColor) == vec3(0,0,0)) discard;
+	//if(vec3(FragColor) == vec3(0,0,0)) discard;
+	if(FragColor.a<0.2){
+		discard;
+	}
 	//FragColor = vec4(1.0f,0.0f,0.0f,1.0f);
 }
